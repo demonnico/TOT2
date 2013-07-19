f3eaa66865f36b5097d996f1c54fdf3fdd35487e
OpenFibers <openfibers@gmail.com>
2013-07-19 10:07:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TOT2::Application.config.secret_token = '3d13567fd6270a9ae1cd41caf8a3289bf20d5831b0abae60297c538e6dbff69848df5c1fee2a892dcb4d5247730775c6678c6e70df2aedc52c3f093f686a1d93'
