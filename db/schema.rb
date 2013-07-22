c26eebb2a4153213d48b5b8ce56c03577a324971
openthread <openfibers@gmail.com>
2013-07-22 22:31:50 +0800

    db schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(:version => 20130722085139) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.boolean  "is_admin"                :default => false
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
