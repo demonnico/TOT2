547287470e69456c0d4c82aa052b9a97fad0ea32
OpenFibers <openfibers@gmail.com>
2013-07-22 11:35:25 +0800

    added redirect of devise login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+
+	# redirect of devise login
+	def after_sign_in_path_for(resource)
+		"/admin"
+	end
+
+	# redirect of devise logout
+	def after_sign_out_path_for(resource_or_scope)
+		#request.referrer #Keeping user on the same page after signing out
+		"/admin"
+	end
 end
