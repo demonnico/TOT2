0434ed84307c2da64060b16c0cebe9b39a03ab45
OpenFibers <openfibers@gmail.com>
2013-07-22 18:30:20 +0800

    hide navigation items when can? manage: @user is false

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
+  def admin?
+    return self.is_admin
+  end
+
   def set_is_admin(boolean)
   	self.is_admin = boolean if !!boolean == boolean #make sure new boolean value is a boolean
   end
