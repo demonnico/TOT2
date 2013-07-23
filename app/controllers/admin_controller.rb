b6cb4564d915354cbb41f63e1adf044a0e2fc90a
openthread <openfibers@gmail.com>
2013-07-24 00:59:15 +0800

    create dir for uploaded files if their dir doesn't exist

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+
+	include AdminHelper
+
 	def apps
 	end
 
@@ -9,8 +12,8 @@ class AdminController < ApplicationController
 		if current_user == nil || !(can? :manage, @app)
 			redirect_to '/admin'
 		end
-			
-		if request.request_method == 'POST' # an upload performed
+
+		if request.request_method == 'POST' # an upload performed with POST method
 
 			notice_string = "Upload successed."
 			alert_string = nil;
@@ -19,6 +22,7 @@ class AdminController < ApplicationController
 			uploaded_ipa_io = params[:ipa] # uploaded ipa file handle
 			if uploaded_ipa_io != nil # if ipa exist, save it to local storage
 				uploaded_ipa_name = Rails.root.join('public', 'uploads', uploaded_ipa_io.original_filename)
+				make_dir_at_path(uploaded_ipa_name)
 				File.open(uploaded_ipa_name, 'wb+') do |file|
 					file.write(uploaded_ipa_io.read)
 				end
@@ -32,6 +36,7 @@ class AdminController < ApplicationController
 				uploaded_dsym_io = params[:dsym] #uploaded dSYM file handle
 				if uploaded_dsym_io != nil #if dSYM exist, save it to local storage
 					uploaded_dsym_name = Rails.root.join('public', 'uploads', uploaded_dsym_io.original_filename)
+					make_dir_at_path(uploaded_dsym_name)
 					File.open(uploaded_dsym_name, 'wb+') do |file|
 						file.write(uploaded_dsym_io.read)
 					end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,11 @@
 module AdminHelper
+
+	# helper method, create dir if it doesn't exist
+	def make_dir_at_path(string)
+		dir = File.dirname(string)
+
+		unless File.directory?(dir)
+			FileUtils.mkdir_p(dir)
+		end
+	end
 end
