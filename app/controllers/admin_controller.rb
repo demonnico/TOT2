73c48962a834ed871ae37780f54ad14362d94637
openthread <openfibers@gmail.com>
2013-07-25 00:29:15 +0800

    redirect to login page from admin page if not login

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,21 @@
 class AdminController < ApplicationController
 
 	layout '_navigation'
+	before_filter :authorize, :only => [:apps]
 
 	def apps
+	end
+
+	#############################################################################
+	# private methods
+
+	private	
+
+	# check access permission
+	def authorize
+		if current_user == nil || !(can? :read, @app)
+			redirect_to '/users/sign_in'
+			return
+		end 
 	end
 end
