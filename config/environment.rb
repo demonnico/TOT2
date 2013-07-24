2572f834e7e9278007d97a8b72b4e8c041989194
openthread <openfibers@gmail.com>
2013-07-24 22:32:35 +0800

    user roles const added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+USER_ROLE_SYSTEM_ADMIN = 0
+USER_ROLE_APP_ADMIN = 1
+USER_ROLE_VIEWER = 2
+
 # Initialize the rails application
 TOT2::Application.initialize!
