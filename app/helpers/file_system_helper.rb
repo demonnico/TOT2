41522a8dfa149112088131a39829afe24471256f
OpenFibers <openfibers@gmail.com>
2013-07-29 14:55:11 +0800

    added unzip method and some comments

--- app/helpers/file_system_helper.rb
+++ app/helpers/file_system_helper.rb
@@ -11,6 +11,7 @@ module FileSystemHelper
 			end
 		end
 
+		#helper method, save io to file
 		def save_io_to_file(io, file_path)
 
 			if io == nil || file_path == nil
@@ -22,6 +23,15 @@ module FileSystemHelper
 			File.open(file_path, 'wb+') do |file|
 				file.write(io.read)
 			end
+		end
+
+		# helper method, zip a zipfile's subfile to destination path
+		# params:
+		# 	zip_file_path : file to unzip
+		# 	unzip_path_hash : a hash contains unzip info.
+		# 		e.g. : unzip zip/a to /tmp/a and unzip zip/b to /tmp/b, pass {"zip/a":"/tmp/a", "zip/b":"tmp/b"}
+		def zip_file_to_destination(zip_file_path, unzip_path_hash)
+			
 		end
 	end
 
