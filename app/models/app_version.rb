c3d44dee7c3e3f03cbbfb5bcbef9736ac208afdc
openthread <openfibers@gmail.com>
2013-07-30 22:58:46 +0800

    changed app_version column

--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -1,4 +1,12 @@
 class AppVersion < ActiveRecord::Base
 	belongs_to :app
-	attr_accessible :app_name, :beta_version, :change_log, :icon_url, :release_date, :short_version, :version
+	attr_accessible :beta_version,
+					:app_name,
+					:version,
+					:short_version, 
+					:release_date,
+					:change_log, 
+					:icon_path, 
+					:itunes_artwork_path
+
 end
