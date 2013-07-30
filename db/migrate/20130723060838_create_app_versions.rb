1de6cf540e2632532ae694e6cad83b3ed064fbe6
openthread <openfibers@gmail.com>
2013-07-30 22:32:44 +0800

    changed app_version migrate

--- db/migrate/20130723060838_create_app_versions.rb
+++ db/migrate/20130723060838_create_app_versions.rb
@@ -1,21 +1,22 @@
 class CreateAppVersions < ActiveRecord::Migration
   def change
     create_table :app_versions do |t|
+      t.integer :beta_version,              :null => false, :default => 0
       t.string :app_name,                   :null => false, :default => ""
       t.string :version,                    :null => false, :default => ""
       t.string :short_version,              :null => false, :default => ""
-      t.integer :beta_version,              :null => false, :default => 0
-      t.string :icon_url,                   :null => false, :default => ""
       t.datetime :release_date
       t.text :change_log,                   :null => false, :default => ""
+      t.string :icon_url
+      t.string :itunes_artwork_url
 
       t.timestamps
     end
 
+    add_index :app_versions, :app_name,                 :unique => false
+    add_index :app_versions, :version,                  :unique => false
+    add_index :app_versions, :short_version,            :unique => false
     add_index :app_versions, :beta_version,             :unique => true
-    add_index :app_versions, :app_name,                 :unique => true
-    add_index :app_versions, :version,                  :unique => true
-    add_index :app_versions, :short_version,            :unique => true
     add_index :app_versions, :release_date,             :unique => true
   end
 end
