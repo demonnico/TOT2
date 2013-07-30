e1c30aea9ed5869f1cfe045282c3ca6d4aed0386
OpenFibers <openfibers@gmail.com>
2013-07-30 17:23:15 +0800

    app_version belongs to app

--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -1,4 +1,4 @@
 class AppVersion < ActiveRecord::Base
-	belongs_to :post
+	belongs_to :app
 	attr_accessible :app_name, :beta_version, :change_log, :icon_url, :release_date, :short_version, :version
 end
