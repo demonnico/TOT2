40a52be0684ae417e760bd140446a10ddc7a6107
OpenFibers <openfibers@gmail.com>
2013-07-31 16:53:18 +0800

    change authorize of app view and delete

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,6 +1,7 @@
 class AppController < ApplicationController
 
 	layout '_navigation'
+	before_filter :view_authorize, :only => [:apps]
 	before_filter :authorize, :only => [:deleteapp]
 
 	def apps
@@ -23,8 +24,16 @@ class AppController < ApplicationController
 	private	
 
 	# check access permission
+	def view_authorize
+		if current_user == nil
+			redirect_to '/users/sign_in'
+		end
+	end
+
 	def authorize
-		if current_user == nil || !(can? :manage, @app)
+		if current_user == nil
+			redirect_to '/users/sign_in'
+		elsif !(can? :manage, @app)
 			flash[:notice] = "You don't have permission to manage app"
 			redirect_to '/admin'
 			return
