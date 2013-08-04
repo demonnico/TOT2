76659d879f085d85931e94c9ef69ccdac2178282
openthread <openfibers@gmail.com>
2013-08-05 00:48:39 +0800

    finished defry method

--- app/helpers/pngdefry_helper.rb
+++ app/helpers/pngdefry_helper.rb
@@ -3,7 +3,10 @@ module PngdefryHelper
     def defry_png(source_path = '')
       pngdefry_path = Rails.root.join('lib', 'myplugin', 'pngdefry', 'pngdefry').to_s
       source_dir = File.dirname(source_path)
-      system(pngdefry_path + ' -o\'' + source_dir + '\' ' + source_path)
+      command_string = pngdefry_path + ' -o\'' + source_dir + '\' \'' + source_path + '\''
+      system(command_string)
+
+      # Rails.logger.info("pngcrush executed: " + command_string)
     end
   end
 end
