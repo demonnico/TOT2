641e05ebf05b19c7bfdd127eea0cfb95940d2527
openthread <openfibers@gmail.com>
2013-08-05 00:41:05 +0800

    added pngdefry helper method

--- app/helpers/pngdefry_helper.rb
+++ app/helpers/pngdefry_helper.rb
@@ -1,2 +1,9 @@
 module PngdefryHelper
+  class << self
+    def defry_png(source_path = '')
+      pngdefry_path = Rails.root.join('lib', 'myplugin', 'pngdefry', 'pngdefry').to_s
+      source_dir = File.dirname(source_path)
+      system(pngdefry_path + ' -o\'' + source_dir + '\' ' + source_path)
+    end
+  end
 end
