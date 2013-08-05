29d5ee5a017ddc353f9d1b1199bdfb36c1f189c7
OpenFibers <openfibers@gmail.com>
2013-08-05 11:34:43 +0800

    added version not found situation to version detail action

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -10,17 +10,16 @@ class AppController < ApplicationController
 
   def version_detail
     version_id = params[:version_id]
-    @back_uri = params[:back_uri]
+    @back_uri = params[:back_uri] || '/'
     if version_id == nil
       redirect_to '/'
     end
     @app_version = AppVersion.find_by_id(version_id)
     if !@app_version
-      redirect_to '/'
-      return
+      @navigation_title = 'Version Not Found'
+    else
+      @navigation_title = 'Install ' + @app_version.app_name
     end
-
-    @navigation_title = 'Install ' + @app_version.app_name
   end
 
   def more_version
