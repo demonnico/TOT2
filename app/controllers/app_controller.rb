75d413d90f8c1e3de47e2f0751597a48c216ddb0
OpenFibers <openfibers@gmail.com>
2013-08-05 10:55:53 +0800

    finished version detail action

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -9,6 +9,18 @@ class AppController < ApplicationController
   end
 
   def version_detail
+    version_id = params[:version_id]
+    @back_uri = params[:back_uri]
+    if version_id == nil
+      redirect_to '/'
+    end
+    @app_version = AppVersion.find_by_id(version_id)
+    if !@app_version
+      redirect_to '/'
+      return
+    end
+
+    @navigation_title = 'Install ' + @app_version.app_name
   end
 
   def more_version
